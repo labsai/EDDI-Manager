49845efbfc137556e368c43e7fe083a3dd11b087
Ξunit <eunithelp@gmail.com>
2023-05-11 10:24:45 +0100

    Feat: Check for correct key in conversationProperties and display the value as Conversation Properties

--- src/scripts/index.tsx
+++ src/scripts/index.tsx
@@ -7,14 +7,36 @@ import { Router, Route } from 'react-router-dom';
 import { persistor, store } from './store/store';
 import { history } from './history';
 import { PersistGate } from 'redux-persist/integration/react';
+import { ConsentProvider } from 'react-hook-consent';
+
+
 
 render(
+  <ConsentProvider
+    options={{
+      services: [
+        {
+          id: 'eddi',
+          name: 'MyName',
+          scripts: [
+            { id: 'external-script', src: 'https://myscript.com/script.js' },
+            { id: 'inline-code', code: `alert("I am a JavaScript code");` },
+          ],
+          cookies: [{ pattern: 'cookie-name' }],
+          mandatory: true,
+        },
+      ],
+      theme: 'dark',
+    }}
+  >
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
       <Router history={history}>
         <Route component={App} />
       </Router>
     </PersistGate>
-  </Provider>,
+  </Provider>
+  </ConsentProvider>
+  ,
   document.getElementById('root'),
 );
