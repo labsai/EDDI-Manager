75b6e2be502a465f343ae577a0387dc14c6d8ae0
Ξunit <eunithelp@gmail.com>
2023-05-13 04:35:17 +0100

    Refactor: Refactor files and restore them to how they where when styles were not broken

--- src/scripts/components/BotConversationView/ConversationTab/ConversationSteps.styles.ts
+++ src/scripts/components/BotConversationView/ConversationTab/ConversationSteps.styles.ts
@@ -1,148 +1,79 @@
+import { makeStyles } from '@material-ui/core/styles';
 import {
   BLUE_COLOR,
-  BLUE_COLOR_TRANSPARENT,
-  DARK_GREY_COLOR,
-  GREY_BORDER,
-  GREY_COLOR3,
-  LIGHTER_YELLOW_COLOR,
-  LIGHT_BLUE_COLOR2,
-  LIGHT_GREY_COLOR2,
-  LIGHT_GREY_COLOR3,
-  MEDIUM_FONT,
-  MEDIUM_FONT3,
-  SMALL_FONT,
-  SMALL_FONT2,
+  DARK_BLUE_BORDER,
   WHITE_COLOR,
+  GREY_BORDER,
+  LIGHT_BLUE_COLOR,
+  LIGHT_GREY_COLOR,
+  MEDIUM_FONT3,
+  BLUE_BORDER,
   YELLOW_COLOR,
 } from '../../../../styles/DefaultStylingProperties';
-import { makeStyles } from '@material-ui/core/styles';
 
-export const useStyles = makeStyles({
-  content: {
+const useStyles = makeStyles({
+  toggleBox: {
+    display: 'flex',
+  },
+  button: {
     '&:hover': {
       backgroundColor: YELLOW_COLOR,
     },
-    flex: 1,
-    fontSize: MEDIUM_FONT,
-    borderRadius: '18px',
-    width: '100%',
-  },
-  input: {
-    backgroundColor: WHITE_COLOR,
-    borderRadius: '18px',
-    color: DARK_GREY_COLOR,
-    display: 'table',
-    padding: '8px 18px',
-    marginLeft: 'auto',
-    marginTop: '12px',
-    wordWrap: 'break-word',
-    maxWidth: '100%',
-  },
-  quickReplies: {
-    marginLeft: '10px',
-  },
-  quickReply: {
-    backgroundColor: GREY_COLOR3,
-    borderRadius: '18px',
-    color: WHITE_COLOR,
-    display: 'inline-block',
-    whiteSpace: 'pre',
-    padding: '8px 18px',
-    marginTop: '12px',
-    marginRight: '10px',
-  },
-  output: {
-    backgroundColor: LIGHT_BLUE_COLOR2,
-    borderRadius: '18px',
-    display: 'table',
-    padding: '8px 18px',
-    marginTop: '12px',
-    marginLeft: '10px',
-    wordWrap: 'break-word',
-    maxWidth: '100%',
-  },
-  outputImage: {
-    padding: '14px 28px',
-    marginTop: '12px',
-    maxWidth: '100%',
-  },
-  timeContainer: {
-    margin: '0',
-    width: '65px',
-    display: 'flex',
-  },
-  timeArrow: {
     position: 'relative',
-    margin: '22px 0 22px 5px',
-    width: '20px',
-    borderTop: `2px solid ${BLUE_COLOR}`,
-    borderRight: `2px solid ${BLUE_COLOR}`,
-    borderBottom: `2px solid ${BLUE_COLOR}`,
-  },
-  timeSpan: {
-    margin: 'auto 0 auto 2px',
-    width: '30px',
-    fontSize: SMALL_FONT,
-    color: WHITE_COLOR,
-  },
-  arrowLeft: {
-    position: 'absolute',
-    bottom: '-6px',
-    right: '12px',
-    width: 0,
-    height: 0,
-    borderTop: '5px solid transparent',
-    borderBottom: '5px solid transparent',
-    borderRight: `10px solid ${BLUE_COLOR}`,
-  },
-  container: {
-    display: 'flex',
-  },
-  chatStep: {
-    flex: 1,
+    top: '2px',
+    border: BLUE_BORDER,
+    width: '21px',
+    height: '21px',
+    borderRadius: '3px',
+    marginLeft: '4px',
+    color: BLUE_BORDER,
     cursor: 'pointer',
-    paddingBottom: '10px',
-    paddingTop: '10px',
-    minWidth: 0,
-  },
-  titleContainer: {
-    '&:hover': {
-      opacity: '0.5',
+
+    '& svg': {
+      color: BLUE_COLOR,
     },
+  },
+  toggleText: {
+    marginTop: '3px',
+    marginLeft: '2px',
+    color: BLUE_COLOR,
+  },
+  icon: {
+    display: 'table',
+    height: '15px',
+    width: '15px',
+    marginTop: '2px',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  },
+  conversationSettings: {
+    display: 'flex',
+    marginBottom: '5px',
+  },
+  refresh: {
+    '&:hover': {
+      backgroundColor: LIGHT_BLUE_COLOR,
+    },
+    position: 'relative',
+    top: '2px',
+    backgroundColor: BLUE_COLOR,
+    width: '40px',
+    height: '21px',
+    borderRadius: '3px',
+    marginLeft: '6px',
+    border: GREY_BORDER,
     cursor: 'pointer',
+  },
+  toolbar: {
+    display: 'table',
+  },
+  title: {
     fontSize: MEDIUM_FONT3,
     color: WHITE_COLOR,
-    backgroundColor: BLUE_COLOR,
     textAlign: 'center',
-    padding: '5px 0',
-    display: 'flex',
+    margin: '20px 0px 20px 0px',
+    borderBottom: DARK_BLUE_BORDER,
   },
-  titleBox: {
-    width: '50px',
-  },
-  titleText: {
-    flex: 1,
-  },
-  actions: {
-    fontSize: SMALL_FONT2,
-    textAlign: 'center',
-    paddingLeft: '65px',
-  },
-  actionTitle: {
-    textDecoration: 'underline',
-    color: BLUE_COLOR,
-  },
-  action: {
-    color: BLUE_COLOR,
-  },
-  jsonView: {},
-  icon: {},
 });
 
-export const rjvStyles = {
-  rjv: {
-    border: GREY_BORDER,
-    borderRadius: '5px',
-    fontSize: SMALL_FONT2,
-  },
-};
+export default useStyles;
