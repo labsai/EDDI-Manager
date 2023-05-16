81cba5889d10b824ce352d2108993d5342fb19b9
Ξunit <eunithelp@gmail.com>
2023-05-16 23:54:59 +0100

    Feat: Add hook-consent stylings

--- src/scripts/index.tsx
+++ src/scripts/index.tsx
@@ -7,18 +7,27 @@ import { Router, Route } from 'react-router-dom';
 import { persistor, store } from './store/store';
 import { history } from './history';
 import { PersistGate } from 'redux-persist/integration/react';
-import { ConsentBanner, ConsentProvider, ConsentOptions } from 'react-hook-consent';
+import { ConsentBanner, ConsentProvider, ConsentOptions, useConsent } from 'react-hook-consent';
+// styling
+import 'react-hook-consent/dist/styles/style.css';
+
+
+
+
+
+
+
 
 const consentOptions: ConsentOptions = {
   services: [
     {
-      id: 'eddi',
+      id: 'eddi-google-analytics',
       name: 'Google Analytics',
       scripts: [
         { id: 'external-script', src: 'https://www.google-analytics.com/analytics.js' },
       ],
       cookies: [{ pattern: 'cookie-name' }],
-      mandatory: true,
+      mandatory: false,
       description: "We may allow third party service providers to use cookies or similar technologies to collect information about your browsing activities over time and across different websites following your use of the Services.For example, we use Google Analytics, an online analytics service operated by Google LLC(Google Analytics).Google Analytics uses cookies to help us analyze traffic on the Site and enhance your experience when you use the Services.For more information on how Google LLC uses this data, go to https://policies.google.com/privacy",
     },
   ],
