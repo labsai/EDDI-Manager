3c1df4403da2f9695361449449c2f22c6d73c187
Ξunit <eunithelp@gmail.com>
2023-05-23 17:20:04 +0100

    Feat: Add Google Tag Manager to website

--- src/scripts/index.tsx
+++ src/scripts/index.tsx
@@ -28,7 +28,17 @@ const consentOptions: ConsentOptions = {
       ],
       cookies: [{ pattern: 'cookie-name' }],
       mandatory: false,
-      description: "We may allow third party service providers to use cookies or similar technologies to collect information about your browsing activities over time and across different websites following your use of the Services.For example, we use Google Analytics, an online analytics service operated by Google LLC(Google Analytics).Google Analytics uses cookies to help us analyze traffic on the Site and enhance your experience when you use the Services.For more information on how Google LLC uses this data, go to https://policies.google.com/privacy",
+      description: "We may allow third party service providers to use cookies or similar technologies to collect information about your browsing activities over time and across different websites following your use of the Services. For example, we use Google Analytics, an online analytics service operated by Google LLC(Google Analytics). Google Analytics uses cookies to help us analyze traffic on the Site and enhance your experience when you use the Services. For more information on how Google LLC uses this data, go to https://policies.google.com/privacy",
+    },
+    {
+      id: 'eddi-google-tag-manager',
+      name: 'Google Tag Manager',
+      scripts: [
+        { id: 'external-script', src: 'https://cdn.jsdelivr.net/gh/labsai/EDDI-Manager-Dist@1.0.1/vendor/gtag.js' },
+      ],
+      cookies: [{ pattern: 'cookie-name' }],
+      mandatory: false,
+      description: "Google Tag Manager is a tag management system (TMS) that allows us to quickly and easily update measurement codes and related code fragments collectively known as tags on your website or mobile app. Google Tag Manager gives us the ability to add and update your own tags for conversion tracking, site analytics, remarketing, and more. For more information on how Google LLC uses this data, go to https://policies.google.com/privacy",
     },
   ],
   theme: 'dark',
